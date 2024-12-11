d787afbe540a7c9a2bc7b495d288c7b4fa467fec
cyxkat157 <svhbatb0n@gmail.com>
2024-12-11 21:04:56 +0800

    fixed

--- examples/dic.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-path = pathlib.Path(__file__).parent  / 'en_US'
-
-from spylls.hunspell import Dictionary
-
-dictionary = Dictionary.from_files(str(path))
-
-print(dictionary.dic)
-print(dictionary.dic.homonyms('spell'))
